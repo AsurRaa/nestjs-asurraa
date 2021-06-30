ce9d50eb582c5ffd0573017fe0c1153a32eec335
duongkimseng <duong.kim.seng@gmail.com>
2021-06-30 18:37:28 +0700

    remove test from package

--- packages/test/src/index.ts
+++
@@ -1,6 +0,0 @@
-// export public api from here
-// for example:
-// export * from './decorators';
-export * from './test';
-export * from './test/test.module';
-export * from './test/entities/test.entity';
